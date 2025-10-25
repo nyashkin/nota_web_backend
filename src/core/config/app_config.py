a6d413834650a0c73c169526c7322031a5500f0b
nyashkin <alexhanin2003@gmail.com>
2025-10-25 20:33:23 +0300

    feat: add api config

--- src/core/config/app_config.py
+++ src/core/config/app_config.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 
+from src.core.config.config_parts.api_config import ApiConfig
 from src.core.config.config_parts.auth_config import AuthConfig
 from src.core.config.config_parts.db_config import DatabaseConfig
 
 
 class AppConfig(BaseModel):
+    api: ApiConfig = ApiConfig()  # type: ignore
     db: DatabaseConfig = DatabaseConfig()  # type: ignore
     auth: AuthConfig = AuthConfig()  # type: ignore
 
