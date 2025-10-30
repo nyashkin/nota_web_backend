3833ee5a23c7bc8daff330a405af6aa7d7017238
nyashkin <alexhanin2003@gmail.com>
2025-10-30 21:05:43 +0300

    refactor: service categories exception handling

--- src/entities/service_category/exceptions/domain.py
+++ src/entities/service_category/exceptions/domain.py
@@ -11,11 +11,6 @@ class ServiceCategoryCreateException(BaseDomainException):
         return "Service category name is not unique or parent is not exists"
 
 
-class ServiceCategoryUknownException(BaseDomainException):
-    def __str__(self) -> str:
-        return "Service category uknown error"
-
-
 class ServiceCategoryIdAndParentIdCannotBeEqualException(BaseDomainException):
     def __str__(self) -> str:
         return "Service category id and parent id cannot be equal exception"
