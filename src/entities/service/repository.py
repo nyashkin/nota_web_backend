aa9ccca4b7950c5546f12ff498efb47a79c2a133
nyashkin <alexhanin2003@gmail.com>
2026-01-05 22:54:01 +0300

    fix: service greenlen

--- src/entities/service/repository.py
+++ src/entities/service/repository.py
@@ -77,7 +77,10 @@ class ServiceRepository:
             for attr, val in update_service.model_dump(exclude_none=True).items():
                 setattr(service_orm, attr, val)
             await self._session.flush()
-            await self._session.refresh(service_orm, attribute_names=["category"])
+            await self._session.refresh(
+                service_orm,
+                attribute_names=["category", "updated_at", "created_at"],
+            )
             await self._session.commit()
         except IntegrityError:
             raise NotUniqueServiceTitleError(update_service.title)
